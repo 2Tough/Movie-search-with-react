a8c41aeb560e087eb7e935a05dfedba31cce690d
2Tough <vainferro@gmail.com>
2022-12-11 22:06:52 +0300

    Added API const.

--- src/SearchMovies.jsx
+++ src/SearchMovies.jsx
@@ -2,8 +2,21 @@ import React from "react";
 
 export default function SearchMovies(){
     
+    const searchMovies = async (e) => {
+        e.preventDefault();
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=def02cd1541154638a709f437d1c8d97&language=en-US&query=${query}&page=1&include_adult=false`
+
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+
+        }catch(err){
+            console.error(err)
+        }
+    }
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMoviesd}>
             <label htmlFor="query" className="Label">Movie name</label>
             <input className="input" type="text" name="query" placeholder="i.e. Jurassic Park"/>
 
